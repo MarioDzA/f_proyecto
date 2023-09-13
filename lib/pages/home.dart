4603f07147888af2f4e9c4e161b8ad335f2269b7
examen <examen@CampusUninorte.local>
2023-09-13 11:55:54 -0500

    added home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,23 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage(
     {Key? key,})
     : super(key: key);
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Math text"),
     ),
-    body:  Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Text(key: const Key('TextHomeHello'), "Hello "),
-        ],
-      ),
+    body: const Center(
+      child: Text('Hello')
     ),
     );}
 }
