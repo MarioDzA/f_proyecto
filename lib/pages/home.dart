0875a8d322e07c460d5a78772d391f9f965aa07a
examen <examen@CampusUninorte.local>
2023-09-13 12:12:54 -0500

    hache

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
+import 'package:f_elproyecto/pages/testpage.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage(
@@ -9,10 +11,16 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Math text"),
+        title: const Text("Math test"),
     ),
-    body: const Center(
-      child: Text('Hello')
+    body: Center(
+      child: ElevatedButton(
+        key: const Key("TestStartButton") ,
+        onPressed: (){
+          Get.to(() => const TestPage(
+            key: const Key('TestPage')
+          ));
+        }, child: Text("Begin test"))
     ),
     );}
 }
